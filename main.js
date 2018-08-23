8e63ce621e12a662ff08ba8230fef4438f0ba298
Brian Vuu <brianvuu@gmail.com>
2018-08-22 19:28:40 -0600

    here you go jimmy

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(){      //VAR, FUNCTION
+	//variables
+
+	var name = document.getElementById('name');
+	var startButton = document.getElementById('start-button');
 	
+	name.addEventListener('keyup', function(event){		
+		if (event.target.value !== '') {
+			startButton.className=''
+		} else {
+			startButton.className="hide"
+		} 
+	})
+	//functions
+
+
 });
